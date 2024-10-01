5d218d53dc10e9fa9975ac5dc9711897f1638aca
WDDilay <156734101+WDDilay@users.noreply.github.com>
2024-10-01 22:20:06 +0800

    copy

---
+++ controller/Controller.js
@@ -0,0 +1,53 @@
+const m = {
+    main:(req, res) => {
+        res.render('index');
+    },
+
+    index2:(req, res) => {
+        res.render('index2');
+    },
+
+    about:(req, res) => {
+        res.render('about');
+    },
+    
+
+    contact:(req, res) => {
+        res.render('contact');
+    },
+
+    news:(req, res) => {
+        res.render('news');
+    },
+
+    singlenews:(req, res) => {
+        res.render('single-news');
+    },
+
+    shop:(req, res) => {
+        res.render('shop');
+    },
+
+    checkout:(req, res) => {
+        res.render('checkout');
+    },
+
+    singleproduct:(req, res) => {
+        res.render('single-product');
+    },
+
+    cart:(req, res) => {
+        res.render('cart');
+    },
+
+    e404:(req, res) => {
+        res.render('e404');
+    },
+
+    login:(req, res) => {
+        res.render('login');
+    }
+    
+};
+
+module.exports = m;
---
+++ routes/router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controller/Controller.js');
+
+
+router.get('/', controller.main);
+router.get('/index2', controller.index2);
+router.get('/about', controller.about);
+router.get('/contact', controller.contact);
+router.get('/news', controller.news);
+router.get('/single-news', controller.singlenews);
+router.get('/shop', controller.shop);
+router.get('/checkout', controller.checkout);
+router.get('/single-product', controller.singleproduct);
+router.get('/cart', controller.cart);
+router.get('/404', controller.e404);
+router.get('/login', controller.login);
+
+
+module.exports = router;
